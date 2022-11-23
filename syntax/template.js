8c9efe1151e024a5e1d3acf23b53bdf75e85f4a4
Jaehong Lee <semi4st@naver.com>
2022-11-23 23:42:04 +0900

    7 commit

---
+++ syntax/template.js
@@ -0,0 +1,12 @@
+var name = 'hong';
+var letter = 'Dear'+name+'\n\ \n\
+lorem- '+name+' ipsum.parag hong raph';
+
+var letter = `Dear ${name}
+
+
+lorem- ${name} ipsum.parag hong raph`;
+
+console.log(letter);
+
+var a = '1';
