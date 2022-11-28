d33e5993207f760b5dfbc9ab6808675394edbaf5
Jaehong Lee <semi4st@naver.com>
2022-11-28 19:47:24 +0900

    12 commit

---
+++ syntax/comparison.js
@@ -0,0 +1,8 @@
+console.log(1==1); //true
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //true
+console.log(1===1); //true
+console.log(1===2); //false
+
+//== 2개 사용하지말고 3개 사용
