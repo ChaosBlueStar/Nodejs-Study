31f1305576648a2c70dd5bc1748f08f3f4ac2fc7
Jaehong Lee <semi4st@naver.com>
2022-11-29 22:14:12 +0900

    15 commit

---
+++ syntax/conditional.js
@@ -0,0 +1,14 @@
+console.log('A');
+console.log('B');
+if(false){
+console.log('C1');
+}
+else{
+    console.log('C2');
+}
+console.log('D');
+
+//조건문에 대한 설명
+//node.js 17 조건문 참고
+
+//전에 했던 if문 이라고 생각하면 될듯
