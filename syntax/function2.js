92be75b6d3e65a6e37a6cd2146fadee6f6912b11
Jaehong Lee <semi4st@naver.com>
2022-12-13 16:15:03 +0900

    23 commit

--- syntax/function2.js
+++ syntax/function2.js
@@ -2,6 +2,9 @@ console.log(Math.round(1.6)); //2
 console.log(Math.round(1.4)); //1
 
 function sum(a, b){ //parameter
-    console.log(a+b)
+    console.log('a');
+    return a+b;
+    console.log('b') // 출력 x
 }
 sum(2, 4); //argumnet
+
