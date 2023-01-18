635b74ca59cb8162b93258db82bf51746776f6a1
Jaehong Lee <118726854+ChaosBlueStar@users.noreply.github.com>
2023-01-18 17:06:09 +0900

    34c

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
  
 function templateHTML(title, list, body){
   return `
   <!doctype html>
   <html>
   <head>
-    <title>WEB2 - ${title}</title>
+    <title>WEB1 - ${title}</title>
     <meta charset="utf-8">
   </head>
   <body>
@@ -43,7 +44,7 @@ var app = http.createServer(function(request,response){
           var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
-        })
+        });
       } else {
         fs.readdir('./data', function(error, filelist){
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
@@ -55,24 +56,36 @@ var app = http.createServer(function(request,response){
           });
         });
       }
-    }else if(pathname === '/create'){
+    } else if(pathname === '/create'){
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = templateList(filelist);
         var template = templateHTML(title, list, `
-        <form action="http://localhost:3000/process_create" method="post">
-<p><input type="text" name="title" placeholder="title"></p>
-<p>
-    <textarea name="description" placeholder="descrtiption"></textarea>
-</p>
-<p>
-    <input type="submit">
-</p>
-</form>
+          <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
         `);
         response.writeHead(200);
         response.end(template);
       });
+    } else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description
+      });
+      response.writeHead(200);
+      response.end('success');
     } else {
       response.writeHead(404);
       response.end('Not found');
