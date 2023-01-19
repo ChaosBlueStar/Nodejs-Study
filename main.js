4b62cacdc5384dfae9ae469f8f97f29e32bcd1b2
Jaehong Lee <118726854+ChaosBlueStar@users.noreply.github.com>
2023-01-19 16:14:01 +0900

    35c

--- main.js
+++ main.js
@@ -83,6 +83,10 @@ var app = http.createServer(function(request,response){
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description
+          fs.writeFile(`data/${title}`,description,'utf8',function(err){
+            response.writeHead(302,{location:`/?id=${title}`});
+            response.end('success');  
+          })
       });
       response.writeHead(200);
       response.end('success');
