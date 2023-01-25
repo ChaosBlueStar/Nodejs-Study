014782b85903c9d8a03035c0fe4fe3f24373364f
Jaehong Lee <semi4st@naver.com>
2023-01-25 16:54:31 +0900

    39c

---
+++ syntax/object.js
@@ -0,0 +1,8 @@
+var members = ['hong','semi','derstern'];
+console.log(members[1]); //semi
+
+var rolse = {'programmer':'hong',
+             'designer':'semi',
+             'manager':'derstern'}
+             
+console.log(rolse.designer);
