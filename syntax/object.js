ee752bdac80b01675c2e45f86763d18ed0add40e
Jaehong Lee <semi4st@naver.com>
2023-01-26 22:56:31 +0900

    39c

--- syntax/object.js
+++ syntax/object.js
@@ -1,8 +1,16 @@
 var members = ['hong','semi','derstern'];
 console.log(members[1]); //semi
-
-var rolse = {'programmer':'hong',
+var i = 0;
+while(i < members.length){
+    console.log('array loop',members[i]);
+    i = i + 1;
+}
+var roles = {'programmer':'hong',
              'designer':'semi',
              'manager':'derstern'}
              
-console.log(rolse.designer);
+console.log(roles.designer); //semi
+
+for(var name in roles){
+    console.log('object =>', name, 'value =>', roles[name]);
+}
